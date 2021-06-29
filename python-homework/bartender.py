28b8fd063426d79bb5cf82bf4168e3dca0e8d397
Lea <learanky@gmail.com>
2021-06-29 14:46:39 +0200

    bartender.py is ready

---
+++ python-homework/bartender.py
@@ -0,0 +1,8 @@
+user_age = input("How old are you?):  ")
+drink_order = input("What would yoou like to drink?:  ")
+if user_age < 18 and drink_order=beer:
+    print("You are too young to drink spirit")
+elif user_age > 60 and drink_order=coke:
+    print("Coffein can raise your blood pressure")
+else:
+    print("Here is your [drink_order]")
